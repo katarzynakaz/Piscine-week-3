57e5aa7864e486fed3f698717e5d819440dc21ef
katarzynakaz <l453r3@gmail.com>
2025-10-23 17:05:46 +0100

    script edited

--- script.mjs
+++ script.mjs
@@ -1,50 +1,62 @@
 import { getUserIds } from "./common.mjs";
-import { selectDate } from "./form.mjs";
+// import { selectDate } from "./form.mjs";
 import { getData } from "./storage.mjs";
 
 window.onload = function () {
 	const users = getUserIds();
-	selectDate();
+	// selectDate();
 };
 
 const selectedUser = document.querySelector("#userSelection");
-const displayAgenda = document.querySelector("#displayAgendaBox");
+const displayBookmarks = document.querySelector("#displayBookmarks");
 
 selectedUser.addEventListener("change", (e) => {
 	const selectedUserId = e.target.value;
-	displayAgenda.innerHTML = "";
+	displayABookmarks.innerHTML = "";
 
 	if (selectedUserId === "default") {
-		displayAgenda.textContent = "Please select a user to view their agenda.";
+		displayAgenda.textContent = "Please select a user to view their bookmarks.";
 		return;
 	}
 
 	const userData = getData(selectedUserId);
-	const currentDate = new Date().toISOString().split("T")[0];
+	// const currentDate = new Date().toISOString().split("T")[0];
 
 	if (!userData) {
-		displayAgenda.textContent = "There are no upcoming revisions for this user";
+		displayAgenda.textContent = "There are no upcoming bookmarks for this user";
 		return;
 	}
-	const futureDate = userData.filter((entry) => entry.date >= currentDate);
+	// const futureDate = userData.filter((entry) => entry.date >= currentDate);
 
-	renderAgenda(futureDate);
+	renderBookmarks(userData);
 });
 
-export function renderAgenda(data) {
-	displayAgenda.innerHTML = "";
+export function renderBookmarks(data) {
+	displayBookmarks.innerHTML = "";
 	const list = document.createElement("ol");
-	list.className = "agendaList";
+	list.className = "bookmarkList";
 
 	const sortedData = [...data].sort(
-		(a, b) => new Date(a.date) - new Date(b.date)
+		(a, b) => new Date(b.createdAt) - new Date(a.createdAt)
 	);
 
 	sortedData.forEach((entry) => {
 		const item = document.createElement("li");
-		item.className = "itemAgendaList";
-		item.textContent = `Topic: ${entry.topic}, Date: ${entry.date}`;
+		item.className = "itemBookmark";
+
+		const titleURL = document.createElement("a");
+		titleURL.href = entry.url;
+		titleURL.textContent = entry.title;
+
+		const descriptionText = document.createElement("p");
+		const createdAt = new Date(entry.createdAt);
+
+		item.appendChild(titleURL);
+		item.appendChild(descriptionText);
+		item.appendChild(timestampText);
+		
 		list.appendChild(item);
+
 	});
-	displayAgenda.appendChild(list);
+	displayBookmarks.appendChild(list);
 }
